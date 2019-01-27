8ec2dfce0cdd7f4e5c5af4c6b816ebe8a9e23cff
masonweaver <mweaver1501@gmail.com>
2019-01-26 22:50:50 -0500

    Add working examples in the examples directory

--- EncryptedSocket.py
+++ EncryptedSocket.py
@@ -4,6 +4,8 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+import os
 import socket
 import sys
 
@@ -29,7 +31,7 @@ class EncryptedSocket(object):
 
 	def get_peer_key(self):
 		
-		self.peer_pub = self.s.recv(4096)
+		self.peer_pub = serialization.load_pem_public_key(self.s.recv(4096), default_backend())
 
 	def send_pub_key(self):
 
@@ -39,6 +41,7 @@ class EncryptedSocket(object):
 		if not self.server:
 			self.s.connect(serv_addr)
 			self.exchange_keys()
+		else:
 			raise RuntimeError("connect cannot be called when class is created with server=True")
 
 
@@ -52,6 +55,7 @@ class EncryptedSocket(object):
 		if self.server:
 			conn, addr = self.s.accept()
 			enc_conn = EncryptedSocket(conn, self.priv_file, self.pub_file, server=True)
+			enc_conn.load_keys()
 			enc_conn.exchange_keys()
 			return enc_conn
 		else:
@@ -77,23 +81,8 @@ class EncryptedSocket(object):
 			salt=None,
 			info=b'handshake data',
 			backend=default_backend()).derive(shared_key)
-		n_priv_key = ec.generate_private_key(ec.SECP384R1(), default_backend())
-		self.pub_key = n_priv_key.public_key()
-		if self.server:
-			self.send_pub_key()
-			self.get_peer_key()
-		else:
-			self.get_peer_key()
-			self.send_pub_key()
-		shared_key = n_priv_key.exchange(ec.ECDH(), self.peer_pub)
-		self.shared_secret = HKDF(
-			algorithm=hashes.SHA256(),
-			length=32,
-			salt=None,
-			info=b'handshake data',
-			backend=default_backend()).derive(shared_key)
+		self.shared_secret = derived_key
 
-		self.encrypter = Fernet(self.shared_secret)
 
 	def generate_new_keys(self):
 		with open(self.priv_file, "wb") as priv_file:
@@ -108,11 +97,38 @@ class EncryptedSocket(object):
 								   format=serialization.PublicFormat.SubjectPublicKeyInfo)
 			pub_file.write(pemp)
 
-	def send(self, data):
-		self.s.send(self.encrypter.encrypt(data.encode('UTF-8')))
+	def encrypt_data(self, data):
+		iv = os.urandom(16)
+		data = self.add_padding(data)
+		try:
+			data = data.encode('UTF-8')
+		except AttributeError:
+			pass
+		cipher = Cipher(algorithms.AES(self.shared_secret), modes.CBC(iv), backend=default_backend())
+		encryptor = cipher.encryptor()
+		cipher_text = encryptor.update(data) + encryptor.finalize()
+		cipher_text = iv + cipher_text
+		return cipher_text
 
-	def recv(self, length):
-		return self.encrypter.decrypt(self.s.recv(length))
+	def decrypt_data(self, data):
+		iv = data[:16]
+		data = data[16:]
+		cipher = Cipher(algorithms.AES(self.shared_secret), modes.CBC(iv), backend=default_backend())
+		decryptor = cipher.decryptor()
+		plain_text = decryptor.update(data) + decryptor.finalize()
+		return plain_text
+
+	def send_data(self, data):
+		self.s.send(self.encrypt_data(data))
+
+	def recv_data(self, length):
+		return self.decrypt_data(self.s.recv(length))
 
 	def close(self):
 		self.s.close()
+
+	def add_padding(self, data):
+		if (len(data) % 16 != 0):
+			data = data + " " * (16 - (len(data) % 16))
+
+		return data
